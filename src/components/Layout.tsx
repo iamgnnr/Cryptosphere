3c2eca75725f726580e60c3b9751f77856de87f5
Gunnar <gnnrbachelor@gmail.com>
2023-10-12 09:22:02 -0600

    Add back column styles

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,7 @@
 
 const Layout = ({ children }) => {
   return (
-    // <div className="container mx-auto px-4 sm:px-6 lg:px-8 grid gap-4 gap-x-14 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
-    <div>
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8 grid gap-4 gap-x-14 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
       {children}
     </div>
   );
