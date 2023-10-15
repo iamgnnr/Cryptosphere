424cdeaf1d19ddcf731cdecd253a62b5bdf326a5
Gunnar <gnnrbachelor@gmail.com>
2023-10-15 12:54:51 -0600

    Polish format for numbers

--- src/pages/CoinDetail.tsx
+++ src/pages/CoinDetail.tsx
@@ -136,12 +136,8 @@ const CoinDetail: React.FC = () => {
             <h3 className='text-zinc-200'>14d: {coinData.market_data.price_change_percentage_14d.toLocaleString('en-US', { maximumFractionDigits: 2 })}%</h3>
             <h3 className='text-zinc-200'>30d: {coinData.market_data.price_change_percentage_30d.toLocaleString('en-US', { maximumFractionDigits: 2 })}%</h3>
             <h3 className='text-zinc-200'>Market Cap Change 24hr: {coinData.market_data.market_cap_change_percentage_24h.toLocaleString('en-US', { maximumFractionDigits: 2 })}%</h3>
-
-
-
-
-            <h3 className='text-zinc-200'>Max Supply: {coinData.market_data.max_supply}</h3>
-            <h3 className='text-zinc-200'>Circulating Supply: {coinData.market_data.circulating_supply}</h3>
+            <h3 className='text-zinc-200'>Max Supply: {coinData.market_data.max_supply.toLocaleString('en-US')}</h3>
+            <h3 className='text-zinc-200'>Circulating Supply: {coinData.market_data.circulating_supply.toLocaleString('en-US')}</h3>
           </div>
         </div>
         <Footer />
